5bf23d1c0db419e8167697c42488dc5b8a68d644
Shashank Singh <shashanksingh6688@gmail.com>
2021-05-10 03:25:38 +0530

    implemented share feature

--- app/src/main/java/com/shalatan/devjoke/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/shalatan/devjoke/ui/overview/OverviewFragment.kt
@@ -1,10 +1,18 @@
 package com.shalatan.devjoke.ui.overview
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,9 +21,13 @@ import androidx.viewpager2.widget.ViewPager2
 import com.shalatan.devjoke.R
 import com.shalatan.devjoke.database.JokeDatabase
 import com.shalatan.devjoke.databinding.FragmentOverviewBinding
+import java.io.ByteArrayOutputStream
+
 
 class OverviewFragment : Fragment() {
 
+    private val SHARE_TEXT =
+        "Install https://play.google.com/store/apps/details?id=com.shalatan.devjoke for more such DevJokes and share your DevJokes/Puns with other devs"
     private lateinit var viewModelFactory: OverviewViewModelFactory
     private lateinit var viewModel: OverviewViewModel
     private lateinit var binding: FragmentOverviewBinding
@@ -73,13 +85,22 @@ class OverviewFragment : Fragment() {
             val jokePosition = jokesViewPager.currentItem
             if (isRedActive) {
                 viewModel.deleteJoke(jokePosition)
+                Toast.makeText(requireContext(), "Joke Removed from Favourites", Toast.LENGTH_SHORT)
+                    .show()
                 Log.e("OverviewFragment : ", "deleted joke")
                 makeButtonLikeable()
             } else {
                 viewModel.saveJoke(jokePosition)
+                Toast.makeText(requireContext(), "Joke Added to Favourites", Toast.LENGTH_SHORT)
+                    .show()
                 Log.e("OverviewFragment : ", "inserted joke")
                 makeButtonDisLikeable()
             }
+        }
+
+        //share carView as image
+        binding.shareButton.setOnClickListener {
+            shareCardView(binding.jokesViewer)
         }
 
         return binding.root
@@ -99,6 +120,44 @@ class OverviewFragment : Fragment() {
     private fun makeButtonLikeable() {
         binding.likeButton.setImageResource(R.drawable.ic_heart)
         isRedActive = false
+    }
+
+    /**
+     * function to get bitmap from viewPager and share ias image
+     */
+    private fun shareCardView(view: View) {
+        val bitmap =
+            Bitmap.createBitmap((view.height * 0.95).toInt(), view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) bgDrawable.draw(canvas) else canvas.drawColor(Color.WHITE)
+        view.draw(canvas)
+        val uri = getImageUri(requireContext(), bitmap)
+
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_STREAM, uri)
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            SHARE_TEXT
+        )
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        intent.type = "image/png"
+        startActivity(intent)
+    }
+
+    /**
+     * function to convert the passed bitmap to Uri and return it
+     */
+    private fun getImageUri(inContext: Context, bitmap: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(
+            inContext.contentResolver,
+            bitmap,
+            "DevJoke",
+            null
+        )
+        return Uri.parse(path)
     }
 
 
